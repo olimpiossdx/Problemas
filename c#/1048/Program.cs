0b0b728e16173b3c58f9476694d177e21b2c645e
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2019-12-06 09:35:44 -0300

    adicinando problemas resolvidos com c#

---
+++ c#/1048/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace olamundo
+{
+    class Program
+    {
+        static int percentual(double salario)
+        {
+            if (salario >= 0.0 && salario <= 400.00)
+            {
+                return (15);
+            }
+            if (salario >= 400.01 && salario <= 800.00)
+            {
+                return (12);
+            }
+            if (salario >= 800.01 && salario <= 1200.00)
+            {
+                return (10);
+            }
+            if (salario >= 1200.01 && salario <= 2000.00)
+            {
+                return (7);
+            }
+            return (4);
+        }
+        static void Main(string[] args)
+        {
+            float salario = float.Parse(Console.ReadLine());
+            Console.WriteLine($"Novo salario: {(((percentual(salario) * salario) / 100) + salario).ToString(".00") }");
+            Console.WriteLine($"Reajuste ganho: {((percentual(salario) * salario) / 100).ToString(".00")}");
+            Console.WriteLine($"Em percentual: {percentual(salario)} %");
+        }
+    }
+}
---
+++ c#/1051/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _1051
+{
+    class Program
+    {
+        public static void impostoDeRenda(float salario)
+        {
+            if (salario > 0.00 && salario <= 2000.00)
+            {
+                Console.WriteLine("Isento");
+            }
+            if (salario >= 2000.01 && salario <= 3000.00)
+            {
+                Console.WriteLine($"R$ {((salario - 2000.00) * 0.08).ToString(".00")}");
+            }
+            if (salario >= 3000.01 && salario <= 4500.00)
+            {
+                Console.WriteLine($"R$ {((1000.00 * 0.08) + ((salario - 3000.00) * 0.18)).ToString(".00")}");
+            }
+            if (salario > 4500.00)
+            {
+                Console.WriteLine($"R$ {((1000.00 * 0.08) + (1500 * 0.18) + ((salario - 4500.00) * 0.28)).ToString(".00")}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            float salario = float.Parse(Console.ReadLine());
+            impostoDeRenda(salario);
+        }
+    }
+}
